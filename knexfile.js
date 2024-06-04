8c4c361a11717cb4a54e70b82e0b971731ba7208
Rohit <rohit@Rohits-Air-2.lan>
2024-06-04 12:53:07 +0530

    add migration script

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+require("dotenv").config({ path: ".env" });
+console.log(process.env);
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
+};
---
+++ migrations/20240604012442_create_students_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("students_clone", function (table) {
+    table.increments("id").primary();
+    table.string("name", 255).notNullable().unique();
+    table.smallint("age").notNullable();
+    table.text("department").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("students_clone");
+};
