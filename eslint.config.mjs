677b84b72cffae19544326cf3b8647e77e6610f3
Rohit <rohit@Rohits-Air-2.lan>
2024-06-10 12:17:09 +0530

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+    ignores: ["node_modules/", "dist"],
+  },
+];
