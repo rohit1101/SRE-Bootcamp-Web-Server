210514b276b52a2996825b672518e2d316a79491
Rohit <rohit@Rohits-Air-2.lan>
2024-06-10 11:21:54 +0530

    update units tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,14 +2,13 @@
 
 const request = require("supertest");
 const app = require("../utils/mock-server");
-const { students } = require("../utils/static");
+const { students: staticStudents } = require("../utils/static");
 
 describe("Test students endpoint API", () => {
-  // Test the GET /users endpoint
   it("should return all students", async () => {
     const res = await request(app).get("/v1/students");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual(students);
+    expect(res.body).toEqual(staticStudents);
   });
 
   it("should add new student", async () => {
@@ -22,24 +21,37 @@ describe("Test students endpoint API", () => {
     expect(res.body.department).toEqual(newUser.department);
   });
 
-  // // Test the POST /users endpoint
-  // it("should create a new user", async () => {
-  //   const newUser = { name: "Priya", age: 27, department: "IT" };
-  //   const res = await request(app).post("/users").send(newUser);
-  //   expect(res.statusCode).toEqual(201);
-  //   expect(res.body).toHaveProperty("id");
-  //   expect(res.body.name).toEqual(newUser.name);
-  //   expect(res.body.age).toEqual(newUser.age);
-  //   expect(res.body.department).toEqual(newUser.department);
-  // });
+  it("should delete a student", async () => {
+    const res = await request(app).delete("/v1/students/2");
+    expect(res.statusCode).toEqual(200);
 
-  // // Test the DELETE /users/:id endpoint
-  // it("should delete a user", async () => {
-  //   const res = await request(app).delete("/users/1");
-  //   expect(res.statusCode).toEqual(204);
+    const getRes = await request(app).get("/v1/students");
+    expect(getRes.body).toHaveLength(20);
+  });
 
-  //   const getRes = await request(app).get("/users");
-  //   expect(getRes.body).toHaveLength(3);
-  //   expect(getRes.body.find((user) => user.id === 1)).toBeUndefined();
-  // });
+  it("should return a user by ID", async () => {
+    const res = await request(app).get("/v1/students/1");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: "Alice",
+      age: 29,
+      department: "HR",
+    });
+  });
+
+  it("should return 404 if user not found", async () => {
+    const res = await request(app).get("/v1/students/999");
+    expect(res.statusCode).toEqual(404);
+    expect(res.body).toEqual({ message: "Student not found" });
+  });
+
+  it("should update an existing student", async () => {
+    const updatedUser = { name: "Alice Updated", age: 30, department: "HR" };
+    const res = await request(app).put("/v1/students/1").send(updatedUser);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.name).toEqual(updatedUser.name);
+    expect(res.body.age).toEqual(updatedUser.age);
+    expect(res.body.department).toEqual(updatedUser.department);
+  });
 });
--- utils/mock-server.js
+++ utils/mock-server.js
@@ -1,10 +1,10 @@
-// app.js
-
 const express = require("express");
-const { students } = require("./static");
+const { students: staticStudents } = require("./static");
 const app = express();
 
 app.use(express.json());
+
+let students = staticStudents;
 
 // Get all users
 app.get("/v1/students", (req, res) => {
@@ -18,11 +18,34 @@ app.post("/v1/students", (req, res) => {
   res.status(200).json(newUser);
 });
 
+// Update an user
+app.put("/v1/students/:id", (req, res) => {
+  const studentId = parseInt(req.params.id);
+  const studentItem = students.filter((student) => student.id === studentId);
+  if (studentItem[0].id === studentId) {
+    studentItem[0] = { id: studentId, ...req.body };
+    res.status(200).json(studentItem[0]);
+  } else {
+    res.status(404).send({ message: "Student not found" });
+  }
+});
+
+// Get user by ID
+app.get("/v1/students/:id", (req, res) => {
+  const studentId = parseInt(req.params.id);
+  const student = students.find((student) => student.id === studentId);
+  if (student) {
+    res.status(200).json(student);
+  } else {
+    res.status(404).send({ message: "Student not found" });
+  }
+});
+
 // Delete a user
-app.delete("/users/:id", (req, res) => {
-  const userId = parseInt(req.params.id, 10);
-  users = users.filter((user) => user.id !== userId);
-  res.status(204).send();
+app.delete("/v1/students/:id", (req, res) => {
+  const studentId = parseInt(req.params.id);
+  students = students.filter((student) => student.id !== studentId);
+  res.status(200).json(students);
 });
 
 module.exports = app;
